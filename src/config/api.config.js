92d6e20785be967e3876e94dc5cd0712ed004bb2
xerotonin <lunatic@tuta.io>
2023-08-05 20:44:12 +0700

    Update api.config.js

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,15 +1,14 @@
 module.exports = {
   // Client ID are safe to expose
   client_id:
-    "126409166174-l2unckghm8d5m1gp3deu0uaps5son64f.apps.googleusercontent.com",
+    "715630795280-9cqs7lgu962jrp9vpfr3b442tq13mv0i.apps.googleusercontent.com",
   // Client secret and refresh token are encrypted
   // Using encryption key
   // https://drive.mbaharip.com/gudora-setup
   client_secret:
-    "54f0505d11a8fe04d22ec642cdde4728:a6f911c70b0305a51160a2133a9bd2ada3c120567857002311f17bf29cc69e07d7f1a9ca20fd119d684191aee6e3866a",
+    "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",
   refresh_token:
-    "209b4d2ff44bf877ce0636ddc81cdc9a:4a40e172ba5d6c1ee8daf50389841bd2c9d1153163196da0dab63c07b6f305bedafaa209a9e6adbb8d2377c74ba98cf818b6e73b8f57098686ed0b89bec93b0186047ccaab8804310a2ab4e46069d1cac8c322f0f1e206808ac62c18639610be07559eebad904905ba185cae8ba9fbe9",
-
+    "1//0gP40dhYzL2fRCgYIARAAGBASNwF-L9IrfO-H8V7p7xTPHRQLWfMS66UVG_MBQoxm8FgR9KG9kbXFctJg3lvgb3UDzMUS1z0y69I",
   files: {
     // How many files to show per page
     itemsPerPage: 25,
@@ -20,7 +19,7 @@ module.exports = {
     // Or use folder id to use a specific folder as starting point
     // TODO: Change when final
     // rootFolder: "root",
-    rootFolder: "1KgPV6QB1GYT8fmn2uTfbtr9rDXqcRR0j", // Test folder
+    rootFolder: "0AGsYlTj11PeWUk9PVA", // Test folder
     // Limit breadcrumb to specific depth
     // 0 = Unlimited
     // 1 = Only show current folder
